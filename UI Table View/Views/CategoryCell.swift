afe70013679a76adce558c370e9c84d515fed37f
TahaMagdy <tahamagdy314@gmail.com>
2018-07-24 18:22:11 +0200

    UI setup

---
+++ UI Table View/Views/CategoryCell.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryCell.swift
+//  UI Table View
+//
+//  Created by Taha Magdy on 7/24/18.
+//  Copyright © 2018 Taha Magdy. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle:UILabel!
+    
+
+}

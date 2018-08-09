4f36b35f4a91b84bf7f57fcbdb81ad9ad0f9f9d8
TahaMagdy <tahamagdy314@gmail.com>
2018-08-09 22:59:23 +0200

    car struct

--- UI Table View/Model/Car.swift
+++ UI Table View/Model/Car.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+
+struct Car {
+    
+    private(set) public var name: String
+    private(set) public var price: String
+    private(set) public var imageName: String
+
+    init(name: String, price: String, imageName: String) {
+        self.name = name
+        self.price = price
+        self.imageName = imageName
+    } // end init
+    
+} // end struct
